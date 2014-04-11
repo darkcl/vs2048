f8ca5a1b7ee6f42a09febd891d3991f596e7d5b0
cpuser <cpuser@emmas-Mac-mini.local>
2014-04-11 14:52:10 +0800

    init project

---
+++ src/com/tpcstld/twozerogame/MainActivity.java
@@ -0,0 +1,397 @@
+package com.tpcstld.twozerogame;
+
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.preference.PreferenceManager;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.Window;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.tpcstld.twozerogame.MainGame.OnGameEventListener;
+import com.tpcstld.twozerogame.MainView.OnMainViewEventListener;
+import com.tpcstld.twozerogame.bluetooth.BluetoothChatService;
+import com.tpcstld.twozerogame.bluetooth.DeviceListActivity;
+
+public class MainActivity extends Activity {
+
+	private static final String TAG = "emma";
+	
+    MainView view;
+    public static final String WIDTH = "width";
+    public static final String HEIGHT = "height";
+    public static final String SCORE = "score";
+    public static final String HIGH_SCORE = "high score temp";
+    public static final String UNDO_SCORE = "undo score";
+    public static final String CAN_UNDO = "can undo";
+    public static final String UNDO_GRID = "undo";
+    public static final String GAME_STATE = "game state";
+    public static final String UNDO_GAME_STATE = "undo game state";
+    public static final String MSG_WIN = "win";
+    public static final String MSG_LOSE = "lose";
+    public static final String MSG_SEND = "send";
+    
+    //bluetooth
+    private BluetoothAdapter mBluetoothAdapter = null;
+ // Member object for the chat services
+    private BluetoothChatService mChatService = null;
+ // Layout Views
+//    private TextView mTitle;
+    // Name of the connected device
+    private String mConnectedDeviceName = null;
+ // Key names received from the BluetoothChatService Handler
+    public static final String DEVICE_NAME = "device_name";
+    public static final String TOAST = "toast";
+    // Intent request codes
+    private static final int REQUEST_CONNECT_DEVICE = 1;
+    private static final int REQUEST_ENABLE_BT = 2;
+ // Message types sent from the BluetoothChatService Handler
+    public static final int MESSAGE_STATE_CHANGE = 1;
+    public static final int MESSAGE_READ = 2;
+    public static final int MESSAGE_WRITE = 3;
+    public static final int MESSAGE_DEVICE_NAME = 4;
+    public static final int MESSAGE_TOAST = 5;
+
+ // The Handler that gets information back from the BluetoothChatService
+    private final Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+            case MESSAGE_STATE_CHANGE:
+                Log.i(TAG, "MESSAGE_STATE_CHANGE: " + msg.arg1);
+                switch (msg.arg1) {
+                case BluetoothChatService.STATE_CONNECTED:
+                	Log.d("emma", "STATE_CONNECTED: "+mConnectedDeviceName);
+                	view.updateConnectionStatus(getResources().
+                			getString(R.string.title_connected_to)+mConnectedDeviceName);
+                    view.game.setIsVsMode(true);
+                    view.game.newGame();
+                    view.game.setOnGameEventListener(new OnGameEventListener() {
+						@Override
+						public void onSendTiles() {
+							sendMessageToOtherPlayer(MSG_SEND);
+						}
+						@Override
+						public void onGameWin() {
+							sendMessageToOtherPlayer(MSG_WIN);
+						}
+						@Override
+						public void onGameLose() {
+							sendMessageToOtherPlayer(MSG_LOSE);
+						}
+					});
+                    break;
+                case BluetoothChatService.STATE_CONNECTING:
+                    Log.d("emma", "STATE_CONNECTING...");
+                    view.updateConnectionStatus(getResources().
+                			getString(R.string.title_connecting));
+                    break;
+                case BluetoothChatService.STATE_LISTEN:
+                case BluetoothChatService.STATE_NONE:
+                    Log.d("emma", "CONNECTED");
+                    view.updateConnectionStatus(getResources().
+                			getString(R.string.title_not_connected));
+                    break;
+                }
+                break;
+            case MESSAGE_WRITE:
+                byte[] writeBuf = (byte[]) msg.obj;
+                // construct a string from the buffer
+                String writeMessage = new String(writeBuf);
+                Log.d("emma", "MESSAGE_WRITE:"+writeMessage);
+                break;
+            case MESSAGE_READ:
+                byte[] readBuf = (byte[]) msg.obj;
+                // construct a string from the valid bytes in the buffer
+                String readMessage = new String(readBuf, 0, msg.arg1);
+                Log.d("emma", "MESSAGE_READ:"+readMessage);
+                handleInMessage(readMessage);
+                break;
+            case MESSAGE_DEVICE_NAME:
+                // save the connected device's name
+                mConnectedDeviceName = msg.getData().getString(DEVICE_NAME);
+                Toast.makeText(getApplicationContext(), "Connected to "
+                               + mConnectedDeviceName, Toast.LENGTH_SHORT).show();
+                break;
+            case MESSAGE_TOAST:
+                Toast.makeText(getApplicationContext(), msg.getData().getString(TOAST),
+                               Toast.LENGTH_SHORT).show();
+                break;
+            }
+        }
+    };
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        view = new MainView(getBaseContext());
+        view.setOnMainViewEventListener(new OnMainViewEventListener() {
+			
+			@Override
+			public void onIconScanClicked() {
+				// Launch the DeviceListActivity to see devices and do scan
+	            Intent serverIntent = new Intent(MainActivity.this, DeviceListActivity.class);
+	            startActivityForResult(serverIntent, REQUEST_CONNECT_DEVICE);
+			}
+			
+			@Override
+			public void onIconDiscoverableClicked() {
+				// Ensure this device is discoverable by others
+	            ensureDiscoverable();
+			}
+		});
+        
+        SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(this);
+        view.hasSaveState = settings.getBoolean("save_state", false);
+
+        if (savedInstanceState != null) {
+            if (savedInstanceState.getBoolean("hasState")) {
+                load();
+            }
+        }
+        setContentView(view);
+        
+     // Get local Bluetooth adapter
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        // If the adapter is null, then Bluetooth is not supported
+        if (mBluetoothAdapter == null) {
+            Toast.makeText(this, "Bluetooth is not available", Toast.LENGTH_LONG).show();
+            finish();
+            return;
+        }
+        
+    }
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if ( keyCode == KeyEvent.KEYCODE_MENU) {
+            //Do nothing
+            return true;
+        } else if (keyCode == KeyEvent.KEYCODE_DPAD_DOWN) {
+            view.game.move(2);
+            return true;
+        } else if (keyCode == KeyEvent.KEYCODE_DPAD_UP) {
+            view.game.move(0);
+            return true;
+        } else if (keyCode == KeyEvent.KEYCODE_DPAD_LEFT) {
+            view.game.move(3);
+            return true;
+        } else if (keyCode == KeyEvent.KEYCODE_DPAD_RIGHT) {
+            view.game.move(1);
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        savedInstanceState.putBoolean("hasState", true);
+        save();
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        Log.d(TAG, "onActivityResult " + resultCode);
+        switch (requestCode) {
+        case REQUEST_CONNECT_DEVICE:
+            // When DeviceListActivity returns with a device to connect
+            if (resultCode == Activity.RESULT_OK) {
+                // Get the device MAC address
+                String address = data.getExtras()
+                                     .getString(DeviceListActivity.EXTRA_DEVICE_ADDRESS);
+                // Get the BLuetoothDevice object
+                BluetoothDevice device = mBluetoothAdapter.getRemoteDevice(address);
+                // Attempt to connect to the device
+                mChatService.connect(device);
+            }
+            break;
+        case REQUEST_ENABLE_BT:
+            // When the request to enable Bluetooth returns
+            if (resultCode == Activity.RESULT_OK) {
+                // Bluetooth is now enabled, so set up a chat session
+                setupChat();
+            } else {
+                // User did not enable Bluetooth or an error occured
+                Log.d(TAG, "BT not enabled");
+                Toast.makeText(this, R.string.bt_not_enabled_leaving, Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        }
+    }
+    
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // If BT is not on, request that it be enabled.
+        // setupChat() will then be called during onActivityResult
+        if (!mBluetoothAdapter.isEnabled()) {
+            Intent enableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableIntent, REQUEST_ENABLE_BT);
+        // Otherwise, setup the chat session
+        } else {
+            if (mChatService == null) setupChat();
+        }
+    }
+    
+    protected void onPause() {
+        super.onPause();
+        save();
+    }
+    
+    private void save() {
+        SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor editor = settings.edit();
+        Tile[][] field = view.game.grid.field;
+        Tile[][] undoField = view.game.grid.undoField;
+        editor.putInt(WIDTH, field.length);
+        editor.putInt(HEIGHT, field.length);
+        for (int xx = 0; xx < field.length; xx++) {
+            for (int yy = 0; yy < field[0].length; yy++) {
+                if (field[xx][yy] != null) {
+                    editor.putInt(xx + " " + yy, field[xx][yy].getValue());
+                } else {
+                    editor.putInt(xx + " " + yy, 0);
+                }
+
+                if (undoField[xx][yy] != null) {
+                    editor.putInt(UNDO_GRID + xx + " " + yy, undoField[xx][yy].getValue());
+                } else {
+                    editor.putInt(UNDO_GRID + xx + " " + yy, 0);
+                }
+            }
+        }
+        editor.putLong(SCORE, view.game.score);
+        editor.putLong(HIGH_SCORE, view.game.highScore);
+        editor.putLong(UNDO_SCORE, view.game.lastScore);
+        editor.putBoolean(CAN_UNDO, view.game.canUndo);
+        editor.putInt(GAME_STATE, view.game.gameState);
+        editor.putInt(UNDO_GAME_STATE, view.game.lastGameState);
+        editor.commit();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        load();
+     // Performing this check in onResume() covers the case in which BT was
+        // not enabled during onStart(), so we were paused to enable it...
+        // onResume() will be called when ACTION_REQUEST_ENABLE activity returns.
+        if (mChatService != null) {
+            // Only if the state is STATE_NONE, do we know that we haven't started already
+            if (mChatService.getState() == BluetoothChatService.STATE_NONE) {
+              // Start the Bluetooth chat services
+              mChatService.start();
+            }
+        }
+        
+        
+    }
+    
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        // Stop the Bluetooth chat services
+        if (mChatService != null) mChatService.stop();
+    }
+
+    private void load() {
+        //Stopping all animations
+        view.game.aGrid.cancelAnimations();
+
+        SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(this);
+        for (int xx = 0; xx < view.game.grid.field.length; xx++) {
+            for (int yy = 0; yy < view.game.grid.field[0].length; yy++) {
+                int value = settings.getInt(xx + " " + yy, -1);
+                if (value > 0) {
+                    view.game.grid.field[xx][yy] = new Tile(xx, yy, value);
+                } else if (value == 0) {
+                    view.game.grid.field[xx][yy] = null;
+                }
+
+                int undoValue = settings.getInt(UNDO_GRID + xx + " " + yy, -1);
+                if (undoValue > 0) {
+                    view.game.grid.undoField[xx][yy] = new Tile(xx, yy, undoValue);
+                } else if (value == 0) {
+                    view.game.grid.undoField[xx][yy] = null;
+                }
+            }
+        }
+
+        view.game.score = settings.getLong(SCORE, view.game.score);
+        view.game.highScore = settings.getLong(HIGH_SCORE, view.game.highScore);
+        view.game.lastScore = settings.getLong(UNDO_SCORE, view.game.lastScore);
+        view.game.canUndo = settings.getBoolean(CAN_UNDO, view.game.canUndo);
+        view.game.gameState = settings.getInt(GAME_STATE, view.game.gameState);
+        view.game.lastGameState = settings.getInt(UNDO_GAME_STATE, view.game.lastGameState);
+    }
+    
+    /*
+     * Bluetooth connection
+     */
+    private void setupChat() {
+        Log.d(TAG, "setupChat()");
+
+     // Initialize the BluetoothChatService to perform bluetooth connections
+        mChatService = new BluetoothChatService(this, mHandler);
+    }
+    
+    private void ensureDiscoverable() {
+        Log.d(TAG, "ensure discoverable");
+        if (mBluetoothAdapter.getScanMode() !=
+            BluetoothAdapter.SCAN_MODE_CONNECTABLE_DISCOVERABLE) {
+            Intent discoverableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+            discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 300);
+            startActivity(discoverableIntent);
+        }
+    }
+
+    /**
+     * Sends a message.
+     * @param message  A string of text to send.
+     */
+    private void sendMessageToOtherPlayer(String message) {
+        // Check that we're actually connected before trying anything
+        if (mChatService.getState() != BluetoothChatService.STATE_CONNECTED) {
+            Toast.makeText(this, R.string.not_connected, Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        // Check that there's actually something to send
+        if (message.length() > 0) {
+            // Get the message bytes and tell the BluetoothChatService to write
+            byte[] send = message.getBytes();
+            Log.d("emma", "onSendTiles() callback -> sendMessageToOtherPlayer("+message+")");
+            mChatService.write(send);
+
+        }
+    }
+    
+    private void handleInMessage(String msg){
+    	if(msg == null || msg.length() < 1){
+    		return;
+    	}
+    	if(msg.equals(MSG_WIN)){
+    		Log.d("emma", "handleInMessage loseInVSMode()");
+    		view.game.loseInVSMode();
+    	}else if(msg.equals(MSG_LOSE)){
+    		Log.d("emma", "handleInMessage winInVSMode()");
+    		view.game.winInVSMode();
+    	}else if(msg.equals(MSG_SEND)){
+    		Log.d("emma", "handleInMessage recieveRandomTile()");
+    		view.game.recieveRandomTile();
+    		
+    	}
+    }
+    
+}
